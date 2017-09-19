7feceb39a94944d104a67a3de2e43227bf881388
dabliuw22 <wleylop22@gmail.com>
2017-09-19 17:52:43 -0500

    Agregando modificaciones para los eventos y para comportamiento responsive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 $(document).ready(function () {
-	$('#btn-menu').click(function (e) {
-		e.preventDefault();
-		if($(this).hasClass('activo')) {
-			$(this).removeClass('activo');
-			$("#menu-container").hide("slide", {direction: "left"}, 200);
-		} else {
-			$(this).addClass('activo');
-			$("#menu-container").show("slide", {direction: "left"}, 200);
-			$("#main-container").css({"margin-left": 60});
 
+	$("#btn-menu").click(function (e) {
+		e.preventDefault();
+		if(!$(this).hasClass('activo')) {
+			$(this).addClass('activo');
+			$("#menu-container").show("slide", {direction: "left"}, 100);
+			$("#btn-menu-left").removeClass('activo');
+			$("#btn-menu-left").css({"display": "inline"});
+		}
+	});
+
+
+	$("#btn-menu-left").click(function (e) {
+		e.preventDefault();
+		if(!$(this).hasClass('activo')) {
+			$(this).addClass('activo');
+			$("#menu-container").hide("slide", {direction: "left"}, 100);
+			$("#btn-menu").css({"display": "inline"});
+			$("#btn-menu").removeClass('activo');
+		}
+	});
+
+	$(window).resize(function () {
+		if($(document).width() > 568) {
+			$("#menu-container").show("slide", {direction: "left"}, 100);
+			$("#btn-menu").removeClass('activo');
+			$("#btn-menu").css({"display": "none"});
+			$("#btn-menu-left").removeClass('activo');
+			$("#btn-menu-left").css({"display": "none"});
+		} else {
+			$("#menu-container").hide("slide", {direction: "left"}, 100);
+			$("#btn-menu").css({"display": "inline"});
+			$("#btn-menu").removeClass('activo');
+			$("#btn-menu-left").removeClass('activo');
 		}
 	});
 });
